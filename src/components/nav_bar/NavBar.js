510566e9f0648c098f789be27b3684f072ee1e2d
Sam Century <sam100years@hotmail.com>
2024-03-05 18:51:36 -0500

    made some edits to the style

--- src/components/nav_bar/NavBar.js
+++ src/components/nav_bar/NavBar.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './NavBar.css'
+import '../../App.css'
 
 const NavBar = () => {
   return (
-    <div className='navbar-container'>
-        <div className='navbar-title'>Shelf No. 5</div>
+    <div className='nav-container'>
+      <div className='adbar-container'>
+        FREE SHIPPING ON ORDERS OVER $100 (CAD) AND OVER $500 (USD)
+      </div>
+      <div className='navbar-container'>
+          <div className='navbar-title poiret-one-regular'>
+            SHELF 
+            <div className='navbar-title-bold'> #5</div>
+          </div>
+      </div>
     </div>
   )
 }
--- src/views/home_page/HomePage.js
+++ src/views/home_page/HomePage.js
@@ -4,10 +4,13 @@ import './HomePage.css'
 const HomePage = () => {
   return (
     <div className='homepage-container'>
+      <img src={require('../../assets/img/chloe-bolton-R0qthXq3jec-unsplash.jpg')} alt='hello' className='homepage-image'/>
       <div className='homepage-content-container'>
-        <div className='homepage-title-item'>Shelf</div>
-        <div className='homepage-title-item'>Number 5</div>
-        <div className='homepage-title-item'>Ceramics Studio</div>
+        <div className='homepage-content'>
+          <span className='homepage-content-text-heading'>Excellence</span>
+          <span className='homepage-content-text'>The pots are so nice wow. I love it so much</span>
+        </div>
+      
       </div>
 
     </div>
