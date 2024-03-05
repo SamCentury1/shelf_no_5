2886aa18065263da8dee0cf3a8edc70c51b2d4c2
Sam Century <sam100years@hotmail.com>
2024-03-04 19:20:18 -0500

    created the main page

---
+++ src/components/nav_bar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className='navbar-container'>
+        <div className='navbar-title'>Shelf No. 5</div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/views/home_page/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div className='homepage-container'>
+      <div className='homepage-content-container'>
+        <div className='homepage-title-item'>Shelf</div>
+        <div className='homepage-title-item'>Number 5</div>
+        <div className='homepage-title-item'>Ceramics Studio</div>
+      </div>
+
+    </div>
+  )
+}
+
+export default HomePage
